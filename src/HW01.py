00a45a6d7f7bb3ac9c08caa36f328beecff6a629
Raymond Yin <raymondxiwei@gmail.com>
2019-01-29 22:40:20 -0500

    initial commit

---
+++ src/HW01.py
@@ -0,0 +1,22 @@
+"""
+name: Xi Wei Yin
+CWID: 10442986
+url: https://sit.instructure.com/courses/30792/assignments/135604
+description:
+    HW 01: Testing triangle classification
+"""
+
+import unittest
+
+def classify_triangle(a, b, c):
+
+
+
+class TestTriangle(unittest.TestCase):
+    def test_input(self):
+        pass
+
+
+if __name__ == '__main__':
+
+    unittest.main(exit=False)
